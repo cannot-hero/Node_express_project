535c5bfce927c0b093a2640dc679a3bd649e25e3
maxence <mabiao811@foxmail.com>
2022-07-23 01:52:12 +0800

    template replace successfully

--- node/index.js
+++ node/index.js
@@ -27,18 +27,17 @@ console.log("will read file");
  */
 const url = require('url')
 const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%neutrient%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTIONS%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    }
-    return output
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
 }
 // 顶级代码，只执行一次  所以同步的代码执行  不用担心阻塞
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
@@ -58,7 +57,7 @@ const server = http.createServer((req, res) => {
         })
         // 将dataObj中数据替换到模板中
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
-        const output = tempOverview.replace('{%PRODECT_CARDS%}', cardsHtml)
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
         res.end(output)
         // product page
     } else if (pathName === '/product') {
