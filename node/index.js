5fe3cba5ff2b8ddc0eb691d5392ad8503aff6d5a
maxence <mabiao811@foxmail.com>
2022-07-23 01:46:31 +0800

    replace template

--- node/index.js
+++ node/index.js
@@ -26,7 +26,24 @@ console.log("will read file");
  * second start a server
  */
 const url = require('url')
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%neutrient%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTIONS%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    }
+    return output
+}
 // 顶级代码，只执行一次  所以同步的代码执行  不用担心阻塞
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
@@ -34,10 +51,19 @@ const server = http.createServer((req, res) => {
     // send a simple response
     // console.log(req.url)
     const pathName = req.url
-    if (pathName === '/') {
-        res.end('Hello from the server created by shushu')
+    // overview
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        // 将dataObj中数据替换到模板中
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODECT_CARDS%}', cardsHtml)
+        res.end(output)
+        // product page
     } else if (pathName === '/product') {
         res.end('this is PRODUCT')
+        // api
     } else if (pathName === '/api') {
         res.writeHead(200, {
             'Content-type': 'application/json'
@@ -45,6 +71,7 @@ const server = http.createServer((req, res) => {
         // 注意这里是data  而不是productData
         res.end(data)
     } else {
+        // not found
         //      status👇  header 👇
         res.writeHead(404, {
             'Content-type': 'text/html',
