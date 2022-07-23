3de5cf2f1cfd8e85089b29b202465573cbf19dd7
maxence <mabiao811@foxmail.com>
2022-07-23 19:15:56 +0800

    第三方库 slugify

--- node/index.js
+++ node/index.js
@@ -3,6 +3,8 @@
 const fs = require("fs");
 const http = require('http')
 const url = require('url')
+
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
 /*****************files****************/
 /*
@@ -36,6 +38,9 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true)
--- note.md
+++ note.md
@@ -15,3 +15,24 @@ const { query, pathname } = url.parse(req.url, true)
 ## 16 using modules
 
 nodejs每一个文件都被视为 a module
+
+## 18 types of packages and installs
+
+packages: regular dependencies & development dependencies
+
+```js
+// development dependencies
+npm i nodemon --save-dev
+```
+
+```js
+npm i nodemon --global
+```
+
+```js
+npm run xxx
+//实际上是npm执行scripts  然后从devDependencies中找可以运行相应脚本的包
+```
+
+## 19 3rd party modules
+使用第三方库时多看看文档呗
