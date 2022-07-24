c3889e9522265dcb8abf5cad7cb7bf5720584c29
maxence <mabiao811@foxmail.com>
2022-07-24 13:07:31 +0800

    before chapiter 2

--- node/index.js
+++ node/index.js
@@ -1,6 +1,6 @@
 // fs 读取数据和写入数据的函数
 // 导入顺序一般为核心模块，然后是自己的模块
-const fs = require("fs");
+const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
@@ -27,54 +27,64 @@ console.log("will read file");
 */
 /******************server**********************/
 /**
- * first create a server 
+ * first create a server
  * second start a server
  */
 
-
 // 顶级代码，只执行一次  所以同步的代码执行  不用担心阻塞
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	'utf-8'
+)
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	'utf-8'
+)
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	'utf-8'
+)
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }))
 console.log(slugs)
 
 const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true)
-    // overview
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        // 将dataObj中数据替换到模板中
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
-        res.end(output)
-        // product page
-    } else if (pathname === '/product') {
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        const product = dataObj[query.id]
-        const output = replaceTemplate(tempProduct, product)
-        res.end(output)
-        // api
-    } else if (pathname === '/api') {
-        res.writeHead(200, {
-            'Content-type': 'application/json'
-        })
-        // 注意这里是data  而不是productData
-        res.end(data)
-    } else {
-        // not found
-        //      status👇  header 👇
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        })
-        res.end('<h1>Page NOT FOUND</h1>')
-    }
+	const { query, pathname } = url.parse(req.url, true)
+	// overview
+	if (pathname === '/' || pathname === '/overview') {
+		res.writeHead(200, { 'Content-type': 'text/html' })
+		// 将dataObj中数据替换到模板中
+		const cardsHtml = dataObj
+			.map((el) => replaceTemplate(tempCard, el))
+			.join('')
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+		res.end(output)
+		// product page
+	} else if (pathname === '/product') {
+		res.writeHead(200, { 'Content-type': 'text/html' })
+		const product = dataObj[query.id]
+		const output = replaceTemplate(tempProduct, product)
+		res.end(output)
+		// api
+	} else if (pathname === '/api') {
+		res.writeHead(200, {
+			'Content-type': 'application/json',
+		})
+		// 注意这里是data  而不是productData
+		res.end(data)
+	} else {
+		// not found
+		//      status👇  header 👇
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+			'my-own-header': 'hello-world',
+		})
+		res.end('<h1>Page NOT FOUND</h1>')
+	}
 })
 
 server.listen(4000, () => {
-    console.log('listening to requests on port 4000')
+	console.log('listening to requests on port 4000')
 })
--- note.md
+++ note.md
@@ -36,3 +36,153 @@ npm run xxx
 
 ## 19 3rd party modules
 使用第三方库时多看看文档呗
+
+## 20 package version
+
+```js
+  "dependencies": {
+    "slugify": "^1.3.4"
+  }
+// 1 major version   huge new release, code may no longer work with previous version
+// 3 minor version   new features, not including breaking changes, always be backward-compatible
+// 4 patch version   fix bug
+```
+
+
+
+```js
+// 查有没有需要更新的包
+npm outdated
+// 更新某一个包
+npm update xxx
+```
+
+## 22 recap
+
+```js
+//readfiles
+//writefiles
+//require core modules
+//require third party modules
+```
+
+# Introduction to backend web development
+
+## Overview
+
+```txt
+https://www.google.com/maps
+protocol    domain name  resource
+    👇 DNS
+https://216.58.211.206:443
+protocol  IP address    Port number
+	👇 TCP/IP socket connection between browser and server
+	👇
+	HTTP REQUEST
+including: start line  method + request target + HTTP version
+			HTTP request headers
+			request body(only when sending data to server)
+	👇
+	HTTP RESPONSE
+including: start line HTTP version + status code + status message 
+			HTTP response headers
+			request body
+	👇
+	index.html is the first to be loaded
+	scanned for assets: JS CSS images(process is repeated for each file)
+```
+
+### TCP/IP
+
+TCP用于分解请求(break out requests of small chunks called packets)为包
+
+IP用于发包(send and route all of these packedts through internet)
+
+## 27 dynamic websites
+
+数据库连同一个预定的模板，基于数据库的数据来构建用户动态请求的每一个页面
+
+然后将该页面构建成HTML、CSS和JS文件，再发送回浏览器
+
+上述过程又被称为服务端渲染(server-side rendering, SSR)
+
+
+
+API Powerd web 称为客户端渲染(client-side rendering) 
+
+for back-end developers, it's far easier to just build an API， let front-end people to build a site.
+
+## 29 How Node works
+
+**V8** 将代码变成计算机可以运行的语言
+
+**libuv** 实现事件循环，线程池(thread pool)
+
+事件循环用于处理简单的任务，例如执行回调和网络IO
+
+线程池用于更繁重的工作  文件访问或压缩
+
+**http-parser**
+
+**c-ares **  DNS
+
+**OpenSSL** 保存记录
+
+**zlib**  用于压缩
+
+## 30 process threads, threads pool
+
+进程 线程 线程池
+
+node单线程
+
+初始化程序--->执行顶层代码---->require modules---->注册事件回调---->开始事件循环
+
+
+
+有些任务太复杂(heavy and expensive)无法在事件循环中进行，会阻塞整个单线程，因而需要线程池(libuv提供)
+
+
+
+将复杂任务offload到线程池(处理文件的操作dealing with files)
+
+file system api, cryptography(例如缓存密码), compression. DNS lookups
+
+## 31 事件循环
+
+事件循环接收事件，每次有重要的事情发生就会调用必要的回调
+
+> 事件循环进行编排(orchestration)，接收事件，调用其回调函数，并将更昂贵的任务卸载到线程池
+
+事件循环顺序
+
+phase1　过期计时器的回调  expired timer callbacks
+
+phase2    I/O 轮询和I/O回调执行  I/P polling and execution of I/O callbacks (Node中I/O主要指网络和文件访问)
+
+phase 3   setImmediate回调(特殊计时器)，如果想立即处理回调（after I/O轮询和执行阶段），可以用这个
+
+phase 4  close callbacks （web 服务器或websocket关闭时）
+
+
+
+nextTIck() queue and other microtask queue 会在每一个phase中间自动执行
+
+
+
+如何退出？
+
+Node会检查是否有计时器或仍在后台运行的I/O任务(any pending timer or I/O task)，如果没有，则会退出(exit program)，如果有则会进入下一个tick
+
+
+
+**dont block the event loop**
+
+> 1 dont use sync versions of function in fs, crypto and zlib modules in callback functions
+>
+> 2 dont perform complex calculations (e.g. loops inside loops)
+>
+> 3 Be careful with JSON in large objects (maybe need long time to sringify JSON)
+>
+> 4 dont use too complex regular expressions(e.g. nested queantifiers)
+
