d25bc82f0e95577fbbf0c46427e9dabd93a0be74
maxence <mabiao811@foxmail.com>
2022-07-27 22:57:23 +0800

    中间件链式调用

--- 4-NATOURS/routes/tourRoutes.js
+++ 4-NATOURS/routes/tourRoutes.js
@@ -6,13 +6,17 @@ const {
 	updateTour,
 	deleteTour,
 	checkID,
+	checkBody,
 } = require('../controllers/tourController')
 
 const router = express.Router()
 // val 是id的值
 router.param('id', checkID)
-
-router.route('/').get(getAllTours).post(createTour)
+// create a new middleware
+// check if body contains the name and price property
+// if not send back 400
+// add it to the post handle stack
+router.route('/').get(getAllTours).post(checkBody, createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 module.exports = router
