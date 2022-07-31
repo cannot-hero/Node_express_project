9957937e26d58ab2cd7b8d75546c5b4116be9cd0
maxence <mabiao811@foxmail.com>
2022-07-31 19:02:43 +0800

    a simple script for loading json

---
+++ 4-NATOURS/dev-data/data/import-dev-data.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+//server文件处理 database configurations, error handling staff or environment varables
+const dotenv = require('dotenv')
+const Tour = require('../../models/toursModel')
+
+dotenv.config({ path: './development.env' })
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABSE_PASSWORD
+)
+mongoose
+    //连接本地数据库
+    // .connect(process.env.DATABASE_LOCAL, {
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        // console.log(con.connections)
+        console.log('DB connection successful!')
+    })
+// read json file
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+)
+
+// import data into database
+const importData = async () => {
+    try {
+        await Tour.create(tours)
+        console.log('Data sucessfully loaded!')
+    } catch (err) {
+        console.log(err)
+    }
+    process.exit()
+}
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany()
+        console.log('Data sucessfully deleted!')
+    } catch (err) {
+        console.log(err)
+    }
+    process.exit()
+}
+
+if (process.argv[2] === '--import') {
+    importData()
+} else if (process.argv[2] === '--delete') {
+    deleteData()
+}
+// Arguments running this node process
+// console.log(process.argv)
