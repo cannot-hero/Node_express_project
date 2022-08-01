9c1545d97428f79932ab403ba1399401de27fdae
maxence <mabiao811@foxmail.com>
2022-08-01 20:19:23 +0800

    advanced filter

--- 4-NATOURS/controllers/tourController.js
+++ 4-NATOURS/controllers/tourController.js
@@ -6,12 +6,21 @@ const Tour = require('./../models/toursModel')
 
 exports.getAllTours = async (req, res) => {
     try {
+        console.log(req.query)
         // BUIKD QUERY
+        // 1) Filtering
         const queryObj = { ...req.query }
         const excludeFields = ['page', 'limit', 'sort', 'fields']
         excludeFields.forEach(el => delete queryObj[el])
-        console.log(req.query, queryObj)
-        const query = Tour.find(queryObj)
+        // 2) Advanced filtering
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(
+            /\b(gte|gt|lte|lt)\b/g,
+            match => `$${match}`
+        ) // 正则表达式
+        console.log(JSON.parse(queryStr))
+        const query = Tour.find(JSON.parse(queryStr))
+        // {difficulty : 'easy', duration:{$gte : 5}}
         // EXCUTE QUERY
         const tours = await query
         // const query = Tour.find()
