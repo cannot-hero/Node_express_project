41fb0d80fc6fcfd53de574ab7b5dff2d49d9108c
maxence <mabiao811@foxmail.com>
2022-08-07 21:03:55 +0800

    116 add 404 not found error

--- 4-NATOURS/controllers/tourController.js
+++ 4-NATOURS/controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError')
 const Tour = require('./../models/toursModel')
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
@@ -36,6 +37,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id)
+    // 通过发了一个假id 发现await 返回值为null
+    if (!tour) {
+        return next(new AppError('No tour could find with this ID', 404))
+    }
     // Tour.findOne({_id:req.params.id})
     res.status(200).json({
         status: 'success',
@@ -62,6 +67,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
         // 重新验证
         runValidators: true
     })
+    if (!tour) {
+        return next(new AppError('No tour could find with this ID', 404))
+    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -70,7 +78,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     })
 })
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id)
+    const tour = await Tour.findByIdAndDelete(req.params.id)
+    if (!tour) {
+        return next(new AppError('No tour could find with this ID', 404))
+    }
     res.status(204).json({
         status: 'success',
         data: null
