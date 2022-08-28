833d82b549952fc6fc7b063ce044d3c1934982e5
maxence <mabiao811@foxmail.com>
2022-08-28 15:36:49 +0800

    184 mapbox p1

---
+++ 4-NATOURS/public/js/mapbox.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+// js run in client side
+// 为了避免发ajax请求，可以将数据放在html中，让js进行操作
+const locations = JSON.parse(document.getElementById('map').dataset.locations)
+console.log(locations)
