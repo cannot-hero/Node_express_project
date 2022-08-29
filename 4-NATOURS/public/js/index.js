be9f6aefa466c3a1d86c691434dfa9ebf705eacb
maxence <mabiao811@foxmail.com>
2022-08-29 10:16:21 +0800

    189 login 打包+alert

---
+++ 4-NATOURS/public/js/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+// index.js 是入口文件 从用户界面获取数据，委托操作
+import '@babel/polyfill'
+import { login } from './login'
+import { displayMap } from './mapbox'
+
+// 为了避免发ajax请求，可以将数据放在html中，让js进行操作
+// DOMELEMENT
+const mapBox = document.getElementById('map')
+const loginForm = document.querySelector('.form')
+
+// 委派 delegation
+if (mapBox) {
+    const locations = JSON.parse(mapBox.dataset.locations)
+    displayMap(locations)
+}
+
+if (loginForm) {
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        login(email, password)
+    })
+}
--- note.md
+++ note.md
@@ -2721,3 +2721,31 @@ const login = async (email, password) => {
 }
 ```
 
+## 189 login p3
+
+使用parcel将所有js脚本打包成一个bundle文件
+
+
+
+警报
+
+```js
+alert.js
+/* eslint-disable */
+
+export const hideAlert = () => {
+    const el = document.querySelector('.alert')
+    // 删除dom，需要移到父元素，然后删除子元素
+    if (el) el.parentElement.removeChild(el)
+}
+// type is 'success' or 'error'
+export const showAlert = (type, msg) => {
+    // 首先隐藏已存在的警报
+    hideAlert()
+    const markup = `<div class="alert alert--${type}}">${msg}}</div>`
+    // 插入相邻的HTML afterbegin 表示在一开始插入  markup表示想要包含的HTML
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup)
+    window.setTimeout(hideAlert, 5000)
+}
+```
+
